6449bc15c6b03e6dddbc519abd5b45c3be0c7afa
Edmund Lin <elz.work22@gmail.com>
2024-09-04 23:24:18 +0800

    test: use POSTGRES_URL

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ require("dotenv").config(); // Load environment variables from .env file
 
 // Create a new pool of connections using environment variables
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_NAME,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 // Export the query function for executing SQL commands
