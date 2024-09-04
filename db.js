7b40a77930e1a8c791818ff476f09c0ec103d7c7
Edmund Lin <elz.work22@gmail.com>
2024-09-04 23:10:51 +0800

    chore: rename to use POSTGRES as environment variable

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ require("dotenv").config(); // Load environment variables from .env file
 
 // Create a new pool of connections using environment variables
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_NAME,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 // Export the query function for executing SQL commands
