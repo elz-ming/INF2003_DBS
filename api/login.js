a1d0a7fb3ca4a1e4152e0f4d36aebdc1e78ab93a
Edmund Lin <elz.work22@gmail.com>
2024-09-05 18:11:36 +0800

    feat: create an error message with hyperlink for better UX

--- api/login.js
+++ api/login.js
@@ -18,7 +18,10 @@ module.exports = async (req, res) => {
       user.rows.length === 0 ||
       !(await bcrypt.compare(password, user.rows[0].password))
     ) {
-      return res.status(401).send("Invalid credentials.");
+      return res.status(401).send(`
+        <p>Invalid credentials.</p>
+        <a href="/screens/login.html">Go back to the login page</a>
+      `);
     }
 
     // Set the authentication cookie
@@ -37,5 +40,6 @@ module.exports = async (req, res) => {
   } else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.end();
   }
 };
