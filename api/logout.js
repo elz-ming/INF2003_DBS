90882841bcbb529cb26259bd1e596e7f5a27214b
Edmund Lin <elz.work22@gmail.com>
2024-09-05 18:11:19 +0800

    feat: create logout and fetch data function for basic usage

---
+++ api/logout.js
@@ -0,0 +1,25 @@
+const cookie = require("cookie");
+
+console.log("logging out");
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    // Clear the auth cookie by setting it with a past expiry date
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("auth", false, {
+        httpOnly: true, // Keep it secure from client-side access
+        expires: new Date(0), // Set the expiry date to the past to remove the cookie
+        path: "/", // Ensure the cookie path is correct
+      })
+    );
+
+    // Redirect to index.html after successful registration
+    res.writeHead(302, { Location: "/screens/login.html" });
+    res.end();
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.writeHead(405, `Method ${req.method} Not Allowed`);
+    res.end();
+  }
+};
